96e36deaedfe7d4aa1dab495b545676b40538a2b
sagarpatel <srp324@drexel.edu>
2018-06-17 17:14:20 -0400

    Setup Graphql server

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 //Import and initialization
-var express = require('express');
-var request = require('request');
-var cheerio = require('cheerio');
+import express from 'express';
+import request from 'request';
+import cheerio from 'cheerio';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import bodyParser from 'body-parser';
+import schema from './data/schema';
+
+const GRAPHQL_PORT = 8080;
+
 var app = express();
 
 app.use(express.static("."));
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
-app.listen(8080, function(){
-	console.log("Server started on port 8080");
-});
+app.listen(GRAPHQL_PORT, () =>
+  console.log(
+    `Server started on port http://localhost:${GRAPHQL_PORT}/ \n \
+    GraphiQL is running on http://localhost:${GRAPHQL_PORT}/graphiql`
+  )
+);
 
 app.get('/getData', function (req, res) {
     request({
@@ -18,7 +29,6 @@ app.get('/getData', function (req, res) {
         var $ = cheerio.load(body);
 
         var hospitals = [];
-
         $('li[class=searchResults-item]').each(function(i, elem) {
             hospitals[i] = { 
                 name: $('.searchResults-name h5').eq(i).text(),
