d5f06cb17b30bff5f7a610872f14d51e6333f195
sagarpatel <srp324@drexel.edu>
2018-06-17 16:46:39 -0400

    Removed commented function

--- handler.js
+++ handler.js
@@ -1,16 +1,3 @@
-// function getData() {
-//     $.ajax({
-//         url:'https://www.srtr.org/transplant-centers/?&organ=kidney&recipientType=adult&sort=rating&page=1/',
-//         type:'get',
-//         headers: {
-//             'Access-Control-Allow-Origin': '*' 
-//         },
-//         success: function(data){
-//          console.log($(data)); 
-//         }
-//       });
-// }
-
 function getData() {
     $.ajax({
         type: "GET",
