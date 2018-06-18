65ab2e47837a7ea58a907daddd741c9f711df78b
sagarpatel <srp324@drexel.edu>
2018-06-18 14:46:56 -0400

    Renamed resolveFunctions to resolvers

--- api/resolvers.js
+++ api/resolvers.js
@@ -4,7 +4,7 @@ import {v1 as neo4j} from 'neo4j-driver';
 //Create Neo4j Driver Instance
 let driver = neo4j.driver(config.neoHost + ":" + config.neoPort, neo4j.auth.basic(config.neoUser, config.neoPass));
 
-const resolveFunctions = {
+const resolvers = {
 };
 
-export default resolveFunctions;
+export default resolvers;
