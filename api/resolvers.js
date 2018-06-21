11c75018191dfad99785f491f0318a5a5356b737
sagarpatel <srp324@drexel.edu>
2018-06-21 17:06:51 -0400

    Found a bug within Neo4j when querying Hospital.organs

--- api/resolvers.js
+++ api/resolvers.js
@@ -1,25 +1,40 @@
 var config = require('../config.json');
-import {v1 as neo4j} from 'neo4j-driver';
+import { v1 as neo4j } from 'neo4j-driver';
 
 //Create Neo4j Driver Instance
 let driver = neo4j.driver(config.neoHost + ":" + config.neoPort, neo4j.auth.basic(config.neoUser, config.neoPass));
 
 const resolvers = {
     Query: {
-        // here we define the resolver for the movies query, which searches for movies by title
-        // params object contains the values for the substring and limit parameters 
         allHospitals(_, params) {
-          // query Neo4j for matching movies
-          let session = driver.session();
-          let query = "MATCH (h:Hospital) RETURN h"
-          return session.run(query, params)
-          .then(result => { 
-              return result.records.map(record => { 
-                    return record.get("h").properties 
+            let session = driver.session();
+            let query = `MATCH (h:Hospital) RETURN h`
+            return session.run(query, params)
+                .then(result => {
+                    return result.records.map(record => {
+                        return record.get("h").properties
+                    })
                 })
-            })
         },
-      }
+    },
+
+    /* BUG within Neo4j: Running this query deletes the Sandbox
+    Hospital: {
+        organs(_, params) {
+            console.log(_, params);
+            let session = driver.session();
+            let query = `MATCH (h:Hospital {name: "PIEDMONT HOSPITAL"})-[t:TRANSPLANTS]-(o:Organ)
+            RETURN DISTINCT o`
+            return session.run(query, params)
+                .then(result => {
+                    return result.records.map(record => {
+                        console.log(record.get("o").properties);
+                        return record.get("o").properties
+                    })
+                })
+        }
+    }
+    */
 };
 
 export default resolvers;
