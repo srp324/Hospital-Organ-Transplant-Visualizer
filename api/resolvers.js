412bc9986011bd625696328809f1911e2348aa48
sagarpatel <srp324@drexel.edu>
2018-06-21 15:44:57 -0400

    Implemented GraphQL Resolver for Query.allHospitals

--- api/resolvers.js
+++ api/resolvers.js
@@ -5,6 +5,21 @@ import {v1 as neo4j} from 'neo4j-driver';
 let driver = neo4j.driver(config.neoHost + ":" + config.neoPort, neo4j.auth.basic(config.neoUser, config.neoPass));
 
 const resolvers = {
+    Query: {
+        // here we define the resolver for the movies query, which searches for movies by title
+        // params object contains the values for the substring and limit parameters 
+        allHospitals(_, params) {
+          // query Neo4j for matching movies
+          let session = driver.session();
+          let query = "MATCH (h:Hospital) RETURN h"
+          return session.run(query, params)
+          .then(result => { 
+              return result.records.map(record => { 
+                    return record.get("h").properties 
+                })
+            })
+        },
+      }
 };
 
 export default resolvers;
